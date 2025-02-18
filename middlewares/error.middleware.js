263ec5e0a69382e47c715f27e1e5691f60490156
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2025-02-18 20:34:13 +0530

    feat: implement global error handling

---
+++ middlewares/error.middleware.js
@@ -0,0 +1,34 @@
+const errorMiddleware = (err, req, res, next) => {
+  try {
+    let error = { ...err };
+    error.message = err.message;
+    console.error(err);
+
+    // Mongoose bad ObjectId
+    if (err.name === "CastError") {
+      const message = `Resource not found with id of ${err.value}`;
+      error = new ErrorResponse(message, 404);
+    }
+
+    // Mongoose duplicate key
+    if (err.code === 11000) {
+      const message = "Duplicate field value entered";
+      error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose validation error
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((val) => val.message);
+      error = new ErrorResponse(message.join(", "), 400);
+    }
+
+    res.status(error.statusCode || 500).json({
+      success: false,
+      error: error.message || "Server Error",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default errorMiddleware;
